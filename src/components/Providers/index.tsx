b0d80b95892a1e883e85b3cfa7953a377d28b3d1
Adam Kovar <kovaradam1@gmail.com>
2021-03-07 18:56:30 +0100

    :recycle: remove IndexedDB

--- src/components/Providers/index.tsx
+++ src/components/Providers/index.tsx
@@ -1,30 +1,10 @@
 import React from 'react';
 import { IconContext } from 'react-icons/lib';
-import IndexedDBProvider from '../../db/Provider';
-import { initLists } from '../../model/list';
-import useStore from '../../store';
-
-const DBConfig = {
-  objectStores: [
-    {
-      name: 'items',
-      options: { keyPath: 'id', autoIncrement: true },
-      indexes: [{ name: 'id', keyPath: 'id', options: { unique: true } }],
-      data: useStore.getState().items,
-    },
-    {
-      name: 'lists',
-      options: { keyPath: 'id', autoIncrement: true },
-      indexes: [{ name: 'id', keyPath: 'id', options: { unique: true } }],
-      data: initLists,
-    },
-  ],
-};
 
 const Providers: React.FC = ({ children }) => {
   return (
     <IconContext.Provider value={{ className: 'react-icons' }}>
-      <IndexedDBProvider config={DBConfig}>{children}</IndexedDBProvider>
+      {children}
     </IconContext.Provider>
   );
 };
